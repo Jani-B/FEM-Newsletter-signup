7afb81d928a5ba457149a30807902d73ced12cc0
Jani B <aijala.j@gmail.com>
2025-06-15 20:06:44 +0300

    Mobile version display ready

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,48 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div class="container">
+      <div class="container font-roboto-400">
         <div class="image-container">
           <img src="/illustration-sign-up-mobile.svg"></img>
         </div>
-        <div class="first-text">
+        <div class="main-area">
           <div>
-            <h1>Stay Updated!</h1>
+            <h1 class="font-roboto-700">Stay Updated!</h1>
           </div>
-          <div>
+          <div class="first-text">
             <p>Join 60,000+ product managers receiving monthly updates on:</p>
           </div>
-          <div>
-            <ul>
-              <li>Product discovery and building what matters</li>
-              <li>Meassuring to ensure updates are a success</li>
-              <li>And much more!</li>
+          <div class="list-container">
+            <ul class="list">
+              <li>
+                <p>Product discovery and building what matters</p>
+              </li>
+              <li>
+                <p>Meassuring to ensure updates are a success</p>
+              </li>
+              <li>
+                <p>And much more!</p>
+              </li>
             </ul>
           </div>
         </div>
-        <div class="second-text">
-          <p>here comes the email form</p>
+        <div class="register">
+          <form class="form">
+            <label class="label font-roboto-700" for="email">
+              Email address
+            </label>
+            <input
+              class="input-email"
+              type="email"
+              id="email"
+              name="email"
+            ></input>
+            <input
+              class="submit"
+              type="submit"
+              value="Subcribe for monthly newsletter"
+            />
+          </form>
         </div>
       </div>
     </>
