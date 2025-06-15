11f32d11e00f9b85b5ec21d413f02396b93cb18b
Jani B <aijala.j@gmail.com>
2025-06-15 14:26:22 +0300

    first commit with basic structure

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div class="container">
+        <div class="image-container">
+          <img src="/illustration-sign-up-mobile.svg"></img>
+        </div>
+        <div class="first-text">
+          <div>
+            <h1>Stay Updated!</h1>
+          </div>
+          <div>
+            <p>Join 60,000+ product managers receiving monthly updates on:</p>
+          </div>
+          <div>
+            <ul>
+              <li>Product discovery and building what matters</li>
+              <li>Meassuring to ensure updates are a success</li>
+              <li>And much more!</li>
+            </ul>
+          </div>
+        </div>
+        <div class="second-text">
+          <p>here comes the email form</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
